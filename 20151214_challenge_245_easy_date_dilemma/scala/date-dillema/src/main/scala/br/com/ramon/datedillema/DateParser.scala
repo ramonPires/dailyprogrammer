f5e26c64aebed646a89cc5451cfb596f2dc9c722
Ramon Pires da Silva <rmn.pires@gmail.com>
2016-12-30 08:14:34 -0200

    minor

--- 20151214_challenge_245_easy_date_dilemma/scala/date-dillema/src/main/scala/br/com/ramon/datedillema/DateParser.scala
+++ 20151214_challenge_245_easy_date_dilemma/scala/date-dillema/src/main/scala/br/com/ramon/datedillema/DateParser.scala
@@ -7,65 +7,65 @@ class DateParser {
   def parse(input: String): String = {
     val splitedDate = splitDate(input)
     val formatedDate = splitedDate.head.length match {
-      case mdy if mdy <= 2 =>  formatPatternMDY(splitedDate)
-      case ymd if ymd == 4 =>  formatPatternYMD(splitedDate)
+      case mdy if mdy <= 2 => formatPatternMDY(splitedDate)
+      case ymd if ymd == 4 => formatPatternYMD(splitedDate)
     }
     formatedDate
   }
 
-  private def splitDate(date : String): Array[String] = {
-       Array("/","-"," ").foreach { c =>
-         if (date.indexOf(c) > -1) return date.split(c)
-       }
+  private def splitDate(date: String): Array[String] = {
+    Array("/", "-", " ").foreach { c =>
+      if (date.indexOf(c) > -1) return date.split(c)
+    }
     new Array[String](0)
   }
 
-  private def formatPatternYMD(splittedDate : Array[String]) : String ={
-    val year = formateYear(splittedDate(0));
+  private def formatPatternYMD(splittedDate: Array[String]): String = {
+    val year = formateYear(splittedDate(0))
 
-    val month = formateMonth(splittedDate(1));
+    val month = formateMonth(splittedDate(1))
 
-    val day = formateDay(splittedDate(2));
+    val day = formateDay(splittedDate(2))
 
-    year+"-"+month+"-"+day;
+    year + "-" + month + "-" + day
   }
 
-  private def formatPatternMDY(splittedDate : Array[String]) : String = {
-    val month = formateMonth(splittedDate(0));
+  private def formatPatternMDY(splittedDate: Array[String]): String = {
+    val month = formateMonth(splittedDate(0))
 
-    val day = formateDay(splittedDate(1));
+    val day = formateDay(splittedDate(1))
 
-    val year = formateYear(splittedDate(2));
-    year+"-"+month+"-"+day;
+    val year = formateYear(splittedDate(2))
+    year + "-" + month + "-" + day
   }
 
 
-  private def formateMonth(month : String) : String = {
+  private def formateMonth(month: String): String = {
     if (month.length() <= 2) {
-      return formatNumberLowerThan10(month);
+      return formatNumberLowerThan10(month)
     }
-    month;
+    month
   }
 
-  private def formateDay(day : String) : String = {
+  private def formateDay(day: String): String = {
     if (day.length() <= 2) {
-      return formatNumberLowerThan10(day);
+      return formatNumberLowerThan10(day)
     }
-    day;
+    day
   }
 
-  private def formateYear(year : String) : String = {
+  private def formateYear(year: String): String = {
     if (year.length() <= 2) {
-      return "20" + formatNumberLowerThan10(year);
+      return "20" + formatNumberLowerThan10(year)
     }
-    year;
+    year
   }
 
-  private def formatNumberLowerThan10(number : String) : String = {
+  private def formatNumberLowerThan10(number: String): String = {
     if (number.length() == 1) {
-      return "0" + number;
+      return "0" + number
     }
-    number;
+    number
   }
 
 }
